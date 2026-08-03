125f5ec4a4f6a6a05d83d90bafaab177f7160352
Kritika Upadhyay <kritika1532oo8@gmail.com>
2026-08-03 19:21:12 +0530

    Finalized the Basic Structure

--- Expense_Tracker.py
+++ Expense_Tracker.py
@@ -1,1 +1,10 @@
-print("Hello World")
+def Expense_Tracker():
+
+    print("-"*7,"MY EXPENSES","-"*7)
+
+    print("1. Add Expense \n2. View Expenses \n3. Search Expenses \n4. Edit Expense \n5. Delete Expense \n6. View Total Expenses \n7. Exit")
+
+    
+
+
+Expense_Tracker()
